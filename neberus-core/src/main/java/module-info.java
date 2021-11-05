93b676b523b7354135fcafd6814f6115ec335e71
Manuel Klette <mnlk.dev@gmail.com>
2021-11-05 12:17:32 +0100

    update dependencies

--- neberus-core/src/main/java/module-info.java
+++ neberus-core/src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module neberus.core {
     requires jdk.javadoc;
-    requires reflections;
+    requires org.reflections;
     requires org.apache.commons.lang3;
     requires java.logging;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
     requires io.swagger.v3.oas.models;
     requires org.commonmark;
     requires asciidoctorj.api;
