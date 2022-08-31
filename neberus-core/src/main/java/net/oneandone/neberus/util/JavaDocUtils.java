25c26c873e52c56506ca5e61aa5f5de15016022d
Manuel Klette <mnlk.dev@gmail.com>
2022-08-31 11:32:34 +0200

    update dependencies

--- neberus-core/src/main/java/net/oneandone/neberus/util/JavaDocUtils.java
+++ neberus-core/src/main/java/net/oneandone/neberus/util/JavaDocUtils.java
@@ -38,7 +38,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public abstract class JavaDocUtils {
